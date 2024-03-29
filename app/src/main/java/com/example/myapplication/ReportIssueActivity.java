991d0df02ef48e65a8d706591481058a7f0cd0db
Dewang Marya <dewang.marya@student.ufv.ca>
2024-03-29 01:43:39 -0700

    Update ReportIssueActivity

--- app/src/main/java/com/example/myapplication/ReportIssueActivity.java
+++ app/src/main/java/com/example/myapplication/ReportIssueActivity.java
@@ -1,4 +1,18 @@
 package com.example.myapplication;
 
-public class ReportIssueActivity {
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReportIssueActivity extends AppCompatActivity {
+    private EditText issueTitle, issueDescription;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+
+        issueTitle = findViewById(R.id.issueTitleEditText);
+        issueDescription = findViewById(R.id.issueDescriptionEditText);
+    }
 }
